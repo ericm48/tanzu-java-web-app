9310b5c8a09572913b79ca8cc01f5cdb30bbaaef
ericm48 <me_manley@yahoo.com>
2022-04-21 14:41:40 -0600

    -Tweaked .gitignore for vscode.

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -11,7 +11,8 @@ import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
-public class Application {
+public class Application 
+{
 
 	public static void main(String[] args) 
 	{
