774a45d0514ed1852b426bddd382754503eba355
ericm48 <me_manley@yahoo.com>
2022-09-20 16:04:08 -0600

    -Updated ReSync.

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController
 	@RequestMapping("/")
 	public String index() 
 	{
-		String message1 		= "Greetings from Spring Boot + Tanzu!!!! CustomValueBe: |";
+		String message1 		= "Greetings from Spring Boot + Tanzu! The CustomValueBe: |";
 		String message2  		= "| The last part!";		
 		String messageOut 		= null;
 		String messsageFromVar 	= null;
