2e15a069cc9fd04ee7f8a43dd92cab96ac1193c6
ericm48 <me_manley@yahoo.com>
2022-09-21 14:16:53 -0600

    -Updated HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController
 	@RequestMapping("/")
 	public String index() 
 	{
-		String message1 		= "Greetings from Spring Boot + Tanzu! The CustomValueBe: |";
+		String message1 		= "Greetings from Spring Boot + Tanzu! The Custom Value Be: |";
 		String message2  		= "| The last part!";		
 		String messageOut 		= null;
 		String messsageFromVar 	= null;
